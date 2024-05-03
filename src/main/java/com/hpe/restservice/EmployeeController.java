63c1ab8a03f44413dab2c558fff69a07105e0db4
sinan <nanossn1008@gmail.com>
2024-05-03 20:03:20 +0100

    Implemented POST method to add a new employee

--- src/main/java/com/hpe/restservice/EmployeeController.java
+++ src/main/java/com/hpe/restservice/EmployeeController.java
@@ -2,7 +2,10 @@ package com.hpe.restservice;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +16,13 @@ public class EmployeeController {
     @GetMapping(path = "/employees")
     public Employees getEmployees() {
         return employeeManager.getAllEmployees();
+    }
+
+    @PostMapping(path = "/employees")
+    public ResponseEntity<Object> addEmployee (@RequestBody Employee employee) {
+        Integer id = employeeManager.getAllEmployees().getEmployees().size() + 1;
+        employee.setId(id);
+        employeeManager.addEmployee(employee);
+        return ResponseEntity.ok().build();
     }
 }
